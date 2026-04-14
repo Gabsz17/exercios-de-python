7cf4774f3d83b0982603e621b858e5448efd0e35
Gabz <gabrielgss2025@gmail.com>
2026-04-13 23:38:57 -0300

    Reintroduce exercises on dictionaries and data analysis

--- execicios_python/06_execicios_dicionarios.py
+++ execicios_python/06_execicios_dicionarios.py
@@ -1,36 +1,36 @@
 # EXECÍCIO 01 - Atualização de Cadastro de Clientes
 
-# #Dicionário de clientes
-# clientes = {"Lira": 5000, "Alon": 3000, "Julia": 4500}
-# #Atualizando valor de Alon
-# clientes["Alon"] += 1500
-# #Adionando novo cliente
-# clientes["Marcos"] = 2000
-# # #Gerando relatórios
-# for nome, valor in clientes.items():
-#     print(f"Cliente {nome} -- Saldo: R${valor}")
+#Dicionário de clientes
+clientes = {"Lira": 5000, "Alon": 3000, "Julia": 4500}
+#Atualizando valor de Alon
+clientes["Alon"] += 1500
+#Adionando novo cliente
+clientes["Marcos"] = 2000
+# #Gerando relatórios
+for nome, valor in clientes.items():
+    print(f"Cliente {nome} -- Saldo: R${valor}")
 
 # EXECÍCIO 02 - Consulta de Estoque Interativa
 
-# estoque = {"teclado": 50, "mouse": 120, "monitor": 30}
-# produto = input("Digite o nome de um produto: ")
-# produto_estoque = produto.strip().lower()
-# if produto_estoque in estoque:
-#     quantidade = estoque[produto_estoque]
-#     print(f"Existem {quantidade} de {produto_estoque} no estoque!")
-# else:
-#     print  (f"Produto não encontrado no sistema")
+estoque = {"teclado": 50, "mouse": 120, "monitor": 30}
+produto = input("Digite o nome de um produto: ")
+produto_estoque = produto.strip().lower()
+if produto_estoque in estoque:
+    quantidade = estoque[produto_estoque]
+    print(f"Existem {quantidade} de {produto_estoque} no estoque!")
+else:
+    print  (f"Produto não encontrado no sistema")
 
 # EXERCÍCIO 03 - Análise de Faturamento por Região
 
-# vendas_regiao = {"Norte": 15000, "Sul": 22000, "Leste": 18000, "Oeste": 25000}
-# total_faturamento = sum(vendas_regiao.values())
-# quantidade_regioes = len(vendas_regiao)
-# media_faturamento = total_faturamento / quantidade_regioes
-# for regiao, faturamento in vendas_regiao.items():
-#     print (f"A região {regiao} faturou {faturamento}")
-# print (f"O faturamento total foi de: {total_faturamento}")
-# print (f"A média de faturamento das regiões foi de: {media_faturamento}")
+vendas_regiao = {"Norte": 15000, "Sul": 22000, "Leste": 18000, "Oeste": 25000}
+total_faturamento = sum(vendas_regiao.values())
+quantidade_regioes = len(vendas_regiao)
+media_faturamento = total_faturamento / quantidade_regioes
+for regiao, faturamento in vendas_regiao.items():
+    print (f"A região {regiao} faturou {faturamento}")
+print (f"O faturamento total foi de: {total_faturamento}")
+print (f"A média de faturamento das regiões foi de: {media_faturamento}")
 
 # EXERCÍCIO 04 -  Média de Desempenho 
 
@@ -39,4 +39,15 @@ print ("As notas de Paula foram: ", desempenho["Paula"])
 total_notas = sum(desempenho["Paula"])
 media_notas = sum(desempenho["Paula"]) / len(desempenho["Paula"])
 print (f"As notas de Paula foram {desempenho['Paula']}")
-print (f"A soma das motas de paula é: {total_notas}")
+print (f"A soma das notas de paula é: {total_notas}")
+print (f"A média das notas de Paula foi: {media_notas:.2f}")
+
+# EXERCÍCIOS 05 - Limpeza de Banco de Dados
+
+produtos = {"celular": 1500, "camera": 800, "radio": 200, "fone": 100}
+produtos_removidos = produtos.pop("radio")
+print(f"foi removido {produtos_removidos} rádios")
+if "celular" in produtos:
+    print(True)
+else: 
+    print (False)
