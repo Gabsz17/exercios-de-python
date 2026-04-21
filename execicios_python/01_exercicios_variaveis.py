6e6305f3e5be066d6044497d5ef807ed890810d9
Gabz <gabrielgss2025@gmail.com>
2026-04-21 00:17:06 -0300

    01_exercicios_variaveis.py

---
+++ execicios_python/01_exercicios_variaveis.py
@@ -0,0 +1,23 @@
+#EXERCÍCIO 01 - Cálculo de Bônus de Vendas
+
+# faturamento = 50000
+# percentual_bonus = 0.10
+# bonus = faturamento * percentual_bonus
+# faturamento_final = faturamento - bonus
+# print (f"O bonus para os funcionários é de: R${bonus}. O faturamento final da empresa é de: R${faturamento_final}")
+
+#EXERCÍCIO 02 - Controle de Estoque
+
+# estoque = 250
+# vendas = 78
+# chegou_estoque = 100
+# final = estoque - vendas + chegou_estoque
+# print (f"A quantidade final de celulares é: {final}")
+
+#EXERCÍCIO 03 -  Divisão de Cargas
+
+caixas = 1250
+caminhoes = 12
+caminhoes_cheios = caixas // caminhoes
+sobra = caixas % caminhoes
+print (f"{caminhoes_cheios} caixas vão em caminhões cheios, sobrou {sobra} caixas que serão levados em uma viagem menor")
